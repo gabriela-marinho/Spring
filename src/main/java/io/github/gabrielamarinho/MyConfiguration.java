310233e7c0aa46b767519588f71dd9e568f411dc
Gabriela <g.barreto.marinho@gmail.com>
2022-07-26 05:53:25 -0300

    ambiente para cada tipo de produção, development e production

--- src/main/java/io/github/gabrielamarinho/MyConfiguration.java
+++ src/main/java/io/github/gabrielamarinho/MyConfiguration.java
@@ -1,16 +1,28 @@
 package io.github.gabrielamarinho;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
+    }
+
 
 /*
 RETORNA SOMENTE CONFIGURAÇÕES
  */
     @Bean
     public  String applicationName(){
+
         return  "Sistema de Vendas";
     }
 }
